120e4114714d84255159bc72f4814068257136b7
joe8021 <vanhornjoe8@gmail.com>
2019-09-13 20:11:06 -0500

    Final commit, completely reworked code. Moving from repettitive non-dynamic code to dynamic code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,129 +1,161 @@
-//$("#time_remaining").hide();
-$("#option1").hide();
-$("#option2").hide();
-$("#option3").hide();
-$("#correct").hide();
-$("#incorrect").hide();
+$(document).ready(function(){
+    //$("#game").hide();
+    $("#start_button").on("click", trivia.startGame);
+    $("#ans").hide();
+    //$(document).on("click", ".option", trivia.guessChecker);
+})
 
-
-//var timer = document.getElementById("#time_remaining");
-
-//var question_options = [];
-var question_ops = document.getElementById("question");
-var correct = document.getElementById("correct");
-var incorrect = document.getElementById("incorrect");
-var q1_op1 = document.getElementById("option1");
-var q1_op2 = document.getElementById("option2");
-var q1_op3 = document.getElementById("option3");
-var q2_op1 = document.getElementById("option1");
-var q2_op2 = document.getElementById("option2");
-var q2_op3 = document.getElementById("option3");
-var q3_op1 = document.getElementById("option1");
-var q3_op2 = document.getElementById("option2");
-var q3_op3 = document.getElementById("option3");
-var number = 30;
+var number = 20;
 var intervalId;
+var q = 0;
 
-//start time when start button is clicked
-$("#start_button").on("click", function(){
-    $("#start_button").hide();
-    question_ops.textContent = questions.q1;
-    $("#option1").show();
-    $("#option2").show();
-    $("#option3").show();
-    $("#correct").show();
-    $("#incorrect").show();
-    runTimer();
+var trivia = {
+    correct: 0,
+    incorrect: 0,
+    unanswered: 0,
+    currentSet: 0,
+    timer: 30,
+    timerOn: false,
+    timerId: '',
+
+
+    trivArr: [
+      {
+        question: 'Who is actually a chef?',
+        answers: ['Monica', 'Chandler', 'Rachel', 'Ross'],
+        correct: "Monica",
+        questionNum: "q1"
+      },
+      {
+        question: 'What does Joey love to eat?',
+        answers: ['Fish', 'Apples', 'Oranges', 'Sandwhiches'],
+        correct: "Sandwhiches",
+        questionNum: "q2"
+      },
+      {
+        question: 'How many times has Ross been divorced?',
+        answers: ['5', '2', '1', '3'],
+        correct: "3",
+        questionNum: "q3"
+      },
+      {
+        question: 'How many types of towels does Monica have?',
+        answers: ['3', '8', '11', '6'],
+        correct: "11",
+        questionNum: "q4"
+      }
+
+    ],
+      
+
+      startGame: function(){
+          $("#start_button").hide();
+          clearInterval(trivia.timerId);
+          trivia.currentSet = 0;
+          trivia.correct = 0;
+          trivia.incorrect = 0;
+          trivia.unanswered = 0;
+
+          $("#game").show();
+          trivia.nextQuestion();
+
+          //reset game results
+          $("#results").html('');
+
+          
+
+          
+          //console.log(trivia.options.q1.length)
+      },
+
+      decrement: function() {
+        number--;
+        $("#time_remaining").html("<h2>" + number + "</h2>");
+        console.log($("#time-remaining"))
+        if (number === 0) {
+          stop();
+          alert("Time Up!");
+          q++;
+          $("#question").empty();
+          $("#choices").empty();
+          trivia.nextQuestion();
+          number=20;
+          trivia.timerOn = true;
+          //this.runTimer();
+        }
+      },
+
+      runTimer: function() {
+        //console.log("run timer function")
+        clearInterval(intervalId);
+        intervalId = setInterval(this.decrement, 1000);
+      },
+
+      nextQuestion: function(){
+        //console.log("next q function")
+        trivia.timer = 30;
+        trivia.timerOn = true;
+        
+
+        $("#question").append(this.trivArr[q].question)
+        if(trivia.timerOn){
+          this.runTimer();
+        }
+      
+        //dynamically creating answer choices
+        console.log(this.trivArr[q].answers.length);
+        for(i=0;i<this.trivArr[q].answers.length;i++){
+          //.console.log("whats up");
+          // $("#choices").append("<button class='user-answer' id=" + "qs" + i + ">" + this.trivArr[q].answers[i] + "</button>")
+          
+          $("#choices").append(`<button class="user-answer btn btn-outline-light" data-answer="${this.trivArr[q].answers[i]}" id="qs">${this.trivArr[q].answers[i]}</button>`)
+
+
+        }
+        //$("#ans").append(this.trivArr[q].correct)
+
+        //HOW DO I SET A DIFFERENT ID FOR EACH BUTTON DYNAMICALLY 
+
+        
+      },
+    
+}
+
+$(document).on("click", ".user-answer", function(){
+  var dataAnswer = $(this).attr("data-answer")
+  if(trivia.trivArr[q].correct == dataAnswer){
+    alert("correct")
+    trivia.correct++;
+    number=20;
+    trivia.timerOn = true;
+  }
+  else{
+    alert("Wrong, the correct answer is " + trivia.trivArr[q].correct);
+    trivia.incorrect++;
+    number=20;
+    trivia.timerOn = true;
+  }
+
+  q++;
+  //console.log("qqqq");
+  $("#question").empty();
+  $("#choices").empty();
+  trivia.nextQuestion();
+  //console.log("the end")
+
+  $("#correct").html("Correct: " + trivia.correct);
+  $("#incorrect").html("Incorrect: " + trivia.incorrect);
+
+  
 
 })
 
-var correct_answers = 0;
-var incorrect_ans = 0;
-
-//start timer
-function runTimer() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  }
-  //count down by 1 second from number=30
-  function decrement() {
-    number--;
-    $("#time_remaining").html("<h2>" + number + "</h2>");
-    if (number === 0) {
-      stop();
-      alert("Time Up!");
-    }
-  }
-
-  //stop timer when number == 0 and display alert
-  function stop() {
-    clearInterval(intervalId);
-  }
-
-    var questions = {
-        q1: 'Who is actually a chef?',
-        q2: 'What does Joey love to eat?',
-        q3: 'How many times has Ross been divorced?'
-    }
-
-    var answers = {
-        q1: 'Monica',
-        q2: 'Sandwhiches',
-        q3: '3'
-    }
-
-    var answerOptions = {
-        q1: ['Monica', 'Chandler', 'Rachel'],
-        q2: ['Fish', 'Sandwhiches', 'Oranges'],
-        q3: ['5', '2', '3']
-    }
-
-    q1_op1.textContent = answerOptions.q1[0];
-    q1_op2.textContent = answerOptions.q1[1];
-    q1_op3.textContent = answerOptions.q1[2];
-
-    //q1
-    if($("#option1").on("click", function (){
-        alert("Correct!")
-        correct_answers++;
-        correct.textContent = "Correct: " + correct_answers;
-        question_ops.textContent = questions.q2;
-        q2_op1.textContent = answerOptions.q2[0];
-        q2_op2.textContent = answerOptions.q2[1];
-        q2_op3.textContent = answerOptions.q2[2];
-        if($("#option2").on("click", function (){
-            alert("Correct!")
-            correct_answers++;
-            correct.textContent = "Correct: " + correct_answers;
-            question_ops.textContent = questions.q3;
-            q3_op1.textContent = answerOptions.q3[0];
-            q3_op2.textContent = answerOptions.q3[1];
-            q3_op3.textContent = answerOptions.q3[2];
-        }));
-        if($("#option1").on("click", function (){
-            alert("Incorrect!")
-            incorrect_ans++;
-            incorrect.textContent = "Incorrect: " + incorrect_ans;
-        }));
-        if($("#option3").on("click", function (){
-            alert("Incorrect!")
-            incorrect_ans++;
-            incorrect.textContent = "Incorrect: " + incorrect_ans;
-        }));
-    }));
-    else if($("#option2").on("click", function (){
-        alert("Incorrect!")
-        incorrect_ans++;
-        incorrect.textContent = "Incorrect: " + incorrect_ans;
-    }));
-    if($("#option3").on("click", function (){
-        alert("Incorrect!")
-        incorrect_ans++;
-        incorrect.textContent = "Incorrect: " + incorrect_ans;
-    }));
-
-    
-
+// $("#qs0").on("click",function(){
+//   //var currentIndex = $(this).attr('data-question')
+//   //<button id="0data-question=0">Monica</button>
+  
+// });
+//nextQuestion();
 
 
 
